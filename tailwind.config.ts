6e19df26ad4c28f2cdb10099146693974c718979
ibqn <evgen.ibqn@gmail.com>
2024-04-01 09:07:03 +0200

    Update tailwind.config.ts with new colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
@@ -13,8 +13,15 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        dark: "#1b1b1b",
+        light: "#fff",
+        accent: "#7B00D3",
+        accentDark: "#ffdb4d",
+        gray: "#747474",
+      },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
