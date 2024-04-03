1fa1bb00080c1b20409fe1b71507733ec81b6d74
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-04-03 08:16:10 +0200

    experiment with frontmatter

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,5 +1,30 @@
+import { FrontMatter, POSTS_PATH } from "@/utils"
+import fs from "fs"
+import path from "path"
+import { compileMDX } from "next-mdx-remote/rsc"
+
 type BlogProps = { params: { slug: string } }
 
+const components = {
+  Image: () => <div className="bg-red-500">IMAGE</div>,
+}
+
 export default async function BlogPage({ params }: BlogProps) {
-  return <div>My Post: {params.slug}</div>
+  const postFilePath = path.join(POSTS_PATH, `${params.slug}`, `index.mdx`)
+  const source = fs.readFileSync(postFilePath)
+
+  const { content, frontmatter } = await compileMDX<FrontMatter>({
+    source,
+    options: { parseFrontmatter: true },
+    components,
+  })
+
+  return (
+    <div>
+      <div>Slug: {params.slug}</div>
+      <div>Title: {frontmatter.title}</div>
+
+      {content}
+    </div>
+  )
 }
