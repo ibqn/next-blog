f37514c97b3d63e12ff7e1a86d35ced530890a8b
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-04-03 08:16:55 +0200

    show post slugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
-export default function Home() {
+import { getPostSlugs } from "@/utils"
+
+export default async function Home() {
+  const postSlugs = getPostSlugs()
+
+  console.log("postSlugs", postSlugs)
+
   return (
     <main className="flex flex-col items-center justify-between">main</main>
   )
