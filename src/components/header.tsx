455d6093bf8135f67c877a8e4fd761cd1468520b
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-04-06 22:25:16 +0200

    Update z-index of navigation menu in header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -10,7 +10,7 @@ export const Header = (props: Props) => {
     <header className="flex w-full items-center justify-between p-4 px-10">
       <Logo />
 
-      <nav className="fixed right-1/2 top-6 flex w-max translate-x-1/2 items-center gap-4 rounded-full border border-solid border-dark bg-light/80 px-8 py-3 font-medium capitalize backdrop-blur-sm">
+      <nav className="fixed right-1/2 top-6 z-50 flex w-max translate-x-1/2 items-center gap-4 rounded-full border border-solid border-dark bg-light/80 px-8 py-3 font-medium capitalize backdrop-blur-sm">
         <Link href="/">Home</Link>
         <Link href="/about">About</Link>
         <Link href="/contact">Contact</Link>
