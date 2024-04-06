12da5c3961db4de8997c92d862882d736c114e1a
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-04-06 11:43:02 +0200

    Refactor CoverSection component to include latest post metadata and update image rendering

--- src/components/home/cover-section.tsx
+++ src/components/home/cover-section.tsx
@@ -1,5 +1,23 @@
-type Props = {}
+import { FrontMatter } from "@/utils"
+import Image from "next/image"
+
+type Props = {
+  latestPostMetadata: FrontMatter
+}
 
 export const CoverSection = (props: Props) => {
-  return <div>CoverSection</div>
+  const { latestPostMetadata } = props
+  return (
+    <div className="w-full">
+      <article className="relative mx-10 flex h-[85vh] flex-col items-start justify-end">
+        <Image
+          src={latestPostMetadata.image}
+          alt="Cover Image"
+          fill
+          className="h-full w-full rounded-3xl object-cover object-center"
+        />
+        <div className="absolute inset-0 h-full rounded-3xl bg-gradient-to-b from-transparent from-0% to-dark"></div>
+      </article>
+    </div>
+  )
 }
---
+++ src/components/home/index.ts
@@ -0,0 +1,1 @@
+export { CoverSection } from "./cover-section"
