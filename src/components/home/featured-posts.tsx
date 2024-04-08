99ad432eaa7ad8d99f9fa8921b7fb69438437cd4
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-04-08 08:29:06 +0200

    Update FeaturedPosts and include blog layout two

--- src/components/home/featured-posts.tsx
+++ src/components/home/featured-posts.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export const FeaturedPosts = (props: Props) => {
   return (
-    <section className="mt-32 flex w-full flex-col items-center justify-center px-32">
+    <section className="mt-32 flex w-full flex-col items-center justify-center px-10">
       <h2 className="w-full text-4xl font-bold capitalize">Featured Posts</h2>
 
       <div className="mt-16 grid grid-cols-2 grid-rows-6 gap-6">
@@ -16,11 +16,15 @@ export const FeaturedPosts = (props: Props) => {
             featuredPostMetadata={props.featuredPostsMetadata[2]}
           />
         </article>
-        <article className="relative col-span-1 row-span-1 bg-yellow-200">
-          <BlogLayoutTwo />
+        <article className="relative col-span-1 row-span-1">
+          <BlogLayoutTwo
+            featuredPostMetadata={props.featuredPostsMetadata[3]}
+          />
         </article>
-        <article className="relative col-span-1 row-span-1 bg-rose-200">
-          <BlogLayoutTwo />
+        <article className="relative col-span-1 row-span-1">
+          <BlogLayoutTwo
+            featuredPostMetadata={props.featuredPostsMetadata[4]}
+          />
         </article>
       </div>
     </section>
