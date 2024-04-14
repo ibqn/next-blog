f2c38b224590fbeeb4b4f0f651e00b932fc01c1e
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-04-14 12:37:18 +0200

    Refactor FeaturedPosts component to use updated blog layouts

--- src/components/home/featured-posts.tsx
+++ src/components/home/featured-posts.tsx
@@ -1,30 +1,28 @@
 import { type FrontMatter } from "@/utils"
 import { BlogLayoutOne, BlogLayoutTwo } from "@/components/blog"
+import assert from "assert"
 
 type Props = {
   featuredPostsMetadata: FrontMatter[]
 }
 
-export const FeaturedPosts = (props: Props) => {
+export const FeaturedPosts = ({ featuredPostsMetadata }: Props) => {
+  // console.log("featuredPostsMetadata", featuredPostsMetadata)
+  assert(featuredPostsMetadata.length === 3, "Featured posts must have 3 items")
+
   return (
     <section className="mt-32 flex w-full flex-col items-center justify-center px-10">
       <h2 className="w-full text-4xl font-bold capitalize">Featured Posts</h2>
 
-      <div className="mt-16 grid grid-cols-2 grid-rows-6 gap-6">
-        <article className="relative col-span-1 row-span-2">
-          <BlogLayoutOne
-            featuredPostMetadata={props.featuredPostsMetadata[2]}
-          />
+      <div className="mt-16 grid grid-cols-2 grid-rows-2 gap-6">
+        <article className="col-span-1 row-span-2 flex">
+          <BlogLayoutOne postMetadata={featuredPostsMetadata[0]} />
         </article>
-        <article className="relative col-span-1 row-span-1">
-          <BlogLayoutTwo
-            featuredPostMetadata={props.featuredPostsMetadata[3]}
-          />
+        <article className="col-span-1 row-span-1">
+          <BlogLayoutTwo postMetadata={featuredPostsMetadata[1]} />
         </article>
-        <article className="relative col-span-1 row-span-1">
-          <BlogLayoutTwo
-            featuredPostMetadata={props.featuredPostsMetadata[4]}
-          />
+        <article className="col-span-1 row-span-1">
+          <BlogLayoutTwo postMetadata={featuredPostsMetadata[2]} />
         </article>
       </div>
     </section>
