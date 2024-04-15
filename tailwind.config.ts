ec241398e74386798b19215bc7250459891e65c4
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-04-15 18:56:17 +0200

    Update Tailwind config to include forms plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,11 @@
 import type { Config } from "tailwindcss"
+import forms from "@tailwindcss/forms"
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{ts,tsx,mdx}",
+    "./src/components/**/*.{ts,tsx,mdx}",
+    "./src/app/**/*.{ts,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -26,6 +27,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [forms],
 }
+
 export default config
