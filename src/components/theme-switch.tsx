1fca49935eac1c13430fe05f8438d715a21c767f
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-04-16 11:22:40 +0200

    Refactor ThemeSwitch component to include client-side rendering and toggle theme functionality

--- src/components/theme-switch.tsx
+++ src/components/theme-switch.tsx
@@ -1,16 +1,36 @@
 "use client"
 
 import { useTheme } from "next-themes"
-import { SunIcon } from "@/components/icons"
+import { MoonIcon, SunIcon } from "@/components/icons"
+import { useEffect, useState } from "react"
 
 type Props = {}
 
 export const ThemeSwitch = (props: Props) => {
+  const [isClient, setIsClient] = useState(false)
   const { theme, setTheme } = useTheme()
 
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+
+  console.log("theme", theme)
+
+  const switchTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light")
+  }
+
+  if (!isClient) {
+    return null
+  }
+
   return (
-    <button>
-      <SunIcon className="h-6 w-6" />
+    <button onClick={switchTheme}>
+      {theme === "light" ? (
+        <SunIcon className="h-6 w-6" />
+      ) : (
+        <MoonIcon className="h-6 w-6" />
+      )}
     </button>
   )
 }
