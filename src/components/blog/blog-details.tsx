964459d82d490d12ce9384d498b7f9fafad50f8f
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-04-16 19:12:53 +0200

    Add BlogDetails component to display blog post details

---
+++ src/components/blog/blog-details.tsx
@@ -0,0 +1,24 @@
+import type { FrontMatter } from "@/mdx-utils"
+import { format, parseISO } from "date-fns"
+import Link from "next/link"
+import type { ReadTimeResults } from "reading-time"
+
+type Props = {
+  postMetadata: FrontMatter
+  readingTime: ReadTimeResults
+}
+
+export const BlogDetails = ({ postMetadata, readingTime }: Props) => {
+  const { publishedAt, tags } = postMetadata
+
+  const tag = tags[0]
+
+  return (
+    <div className="mx-10 flex flex-row flex-wrap items-center justify-around gap-3 rounded-lg bg-accent px-10 py-2 text-xl font-medium text-light">
+      <time>{format(parseISO(publishedAt), "MMMM dd, yyyy")}</time>
+      <span>10 Views</span>
+      <div>{readingTime.text}</div>
+      <Link href={`/category/${tag}`}>#{tag}</Link>
+    </div>
+  )
+}
--- src/components/blog/index.ts
+++ src/components/blog/index.ts
@@ -1,3 +1,5 @@
 export { BlogLayoutOne } from "./blog-layout-one"
 export { BlogLayoutTwo } from "./blog-layout-two"
 export { BlogLayoutThree } from "./blog-layout-three"
+
+export { BlogDetails } from "./blog-details"
