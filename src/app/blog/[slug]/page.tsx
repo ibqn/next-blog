ad768c7f91703ad1c7ea4819d4e1a435a7af1f1a
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-04-16 19:16:43 +0200

    Update BlogPage component to include image, reading time, and tag information

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -2,6 +2,14 @@ import { type FrontMatter, getPostsPath, getPostSlugs } from "@/mdx-utils"
 import fs from "fs"
 import path from "path"
 import { compileMDX } from "next-mdx-remote/rsc"
+import { Tag } from "@/components/elements"
+import Image from "next/image"
+import { BlogDetails } from "@/components/blog"
+import estimateReadingTime from "reading-time"
+import { cn } from "@/utils"
+import remarkGfm from "remark-gfm"
+import rehypeSlug from "rehype-slug"
+import rehypeAutolinkHeadings from "rehype-autolink-headings"
 
 type BlogProps = { params: { slug: string } }
 
@@ -19,16 +27,64 @@ export default async function BlogPage({ params }: BlogProps) {
 
   const { content, frontmatter } = await compileMDX<FrontMatter>({
     source,
-    options: { parseFrontmatter: true },
+    options: {
+      parseFrontmatter: true,
+      mdxOptions: {
+        remarkPlugins: [remarkGfm],
+        rehypePlugins: [
+          rehypeSlug,
+          [rehypeAutolinkHeadings, { behavior: "append" }],
+        ],
+      },
+    },
     components,
   })
 
-  return (
-    <div>
-      <div>Slug: {params.slug}</div>
-      <div>Title: {frontmatter.title}</div>
+  const { tags, title, image } = frontmatter
 
-      {content}
-    </div>
+  const tag = tags[0]
+
+  const readingTime = estimateReadingTime(source.toString("utf8"))
+
+  return (
+    <article>
+      <div className="relative mb-8 h-[70vh] w-full bg-dark text-center">
+        <Image
+          src={image}
+          width={1200}
+          height={1200}
+          alt={title}
+          className="aspect-square h-full w-full object-cover object-center transition-all duration-300 ease-in-out group-hover:scale-105"
+        />
+        <div className="absolute inset-0 bg-dark/60" />
+
+        <div className="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center">
+          <Tag
+            tag={tag}
+            link={`/category/${tag}`}
+            className="px-6 py-2 text-sm"
+          />
+          <h1 className="relative mt-6 w-5/6 text-5xl font-semibold capitalize leading-normal text-light">
+            {title}
+          </h1>
+        </div>
+      </div>
+
+      <BlogDetails postMetadata={frontmatter} readingTime={readingTime} />
+
+      <div className="mt-8 grid grid-cols-12 gap-16 px-10">
+        <div className="col-span-4">toc</div>
+        <div
+          className={cn(
+            "prose lg:prose-xl  col-span-8 max-w-max font-inter",
+            "prose-blockquote:bg-accent/20 prose-blockquote:p-2 prose-blockquote:px-6 prose-blockquote:border-accent",
+            "prose-blockquote:rounded-r-lg prose-blockquote:not-italic",
+            "prose-li:marker:text-accent"
+          )}
+        >
+          {content}
+        </div>
+      </div>
+    </article>
   )
 }
