530dac007dcf590ee8e28534a0e33034b16f0d14
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-04-16 09:45:08 +0200

    Add SunIcon component to icons/index.ts

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -2,3 +2,5 @@ export { DribbleIcon } from "./dribble"
 export { LinkedinIcon } from "./linkedin"
 export { TwitterIcon } from "./twitter"
 export { GithubIcon } from "./github"
+
+export { SunIcon } from "./sun-icon"
