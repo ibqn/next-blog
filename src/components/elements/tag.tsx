bbdc6e7a06b200caebe30568742ef5b97635be42
ibqn <evgen.ibqn@gmail.com>
2024-04-20 11:07:41 +0200

    Refactor Tag component to use scale on span

--- src/components/elements/tag.tsx
+++ src/components/elements/tag.tsx
@@ -13,12 +13,11 @@ export const Tag = ({ tag, link, className, ...props }: Props) => {
       {...props}
       href={link}
       className={cn(
-        "inline-block rounded-full border border-solid border-light bg-dark px-10 py-3 font-semibold capitalize text-light",
-        "transition-all duration-200 hover:scale-105",
+        "inline-flex rounded-full border border-solid border-light bg-dark px-10 py-3 font-semibold capitalize text-light",
         className
       )}
     >
-      {tag}
+      <span className="transition-all duration-200 hover:scale-105">{tag}</span>
     </Link>
   )
 }
