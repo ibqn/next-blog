59c654b240d23926fa7472016e3861ec59b88ef4
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-10-03 20:25:05 +0200

    refactor: update CategoryProps type and streamline parameter handling in CategoryPage

--- src/app/category/[category]/page.tsx
+++ src/app/category/[category]/page.tsx
@@ -2,7 +2,7 @@ import { BlogLayoutThree, Categories } from "@/components/blog"
 import { getAllCategories, getPostsMetadataByCategory } from "@/mdx-utils"
 import { slug } from "github-slugger"
 
-type CategoryProps = { params: { category: string } }
+type CategoryProps = { params: Promise<{ category: string }> }
 
 export async function generateStaticParams() {
   const categories = await getAllCategories()
@@ -12,7 +12,7 @@ export async function generateStaticParams() {
 }
 
 export async function generateMetadata({ params }: CategoryProps) {
-  const { category } = params
+  const { category } = await params
 
   return {
     title: `${category.replaceAll("-", " ")} Blogs`,
@@ -22,28 +22,22 @@ export async function generateMetadata({ params }: CategoryProps) {
 
 export default async function CategoryPage({ params }: CategoryProps) {
   const categories = await getAllCategories()
+  const { category } = await params
 
-  const filteredPostsMetadata = await getPostsMetadataByCategory(
-    params.category
-  )
+  const filteredPostsMetadata = await getPostsMetadataByCategory(category)
 
   return (
-    <article className="mt-12 flex flex-col text-dark">
+    <article className="text-dark mt-12 flex flex-col">
       <div className="flex flex-col px-32">
-        <h1 className="mt-6 text-5xl font-semibold">#{params.category}</h1>
-        <span className="mt-2">
-          Discover more categories and expand your knowledge!
-        </span>
+        <h1 className="mt-6 text-5xl font-semibold">#{category}</h1>
+        <span className="mt-2">Discover more categories and expand your knowledge!</span>
       </div>
 
-      <Categories categories={categories} currentCategory={params.category} />
+      <Categories categories={categories} currentCategory={category} />
 
       <div className="mx-10 mt-16 grid grid-cols-3 gap-16">
         {filteredPostsMetadata.map((metadata) => (
-          <article
-            key={metadata.slug}
-            className="relative col-span-1 row-span-1"
-          >
+          <article key={metadata.slug} className="relative col-span-1 row-span-1">
             <BlogLayoutThree postMetadata={metadata} />
           </article>
         ))}
