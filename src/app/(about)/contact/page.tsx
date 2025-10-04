ca00b176ce20965628a3ddb1c0650ec90d9703b1
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-10-04 13:06:56 +0200

    feat: add contact image and update ContactPage layout

--- src/app/(about)/contact/page.tsx
+++ src/app/(about)/contact/page.tsx
@@ -1,5 +1,7 @@
 import { ContactForm } from "@/components/contact"
 import { siteMetadata } from "@/utils"
+import Image from "next/image"
+import contactImage from "@/components/contact/contact-image.svg"
 
 type ContactProps = {}
 
@@ -10,9 +12,9 @@ export const metadata = {
 
 export default async function ContactPage(props: ContactProps) {
   return (
-    <section className="mx-10 flex h-[75vh] flex-row items-center justify-center border-b-2 border-solid border-dark text-dark">
-      <div className="flex h-full flex-2 border-r-2 border-solid border-dark">
-        image
+    <section className="border-dark text-dark mx-10 flex h-[75vh] flex-row items-center justify-center border-b-2 border-solid">
+      <div className="border-dark flex h-full flex-2 border-r-2 border-solid">
+        <Image alt="Contact Image" src={contactImage} className="h-full w-full object-contain object-center" />
       </div>
 
       <div className="flex flex-3 flex-col items-start justify-center px-16 pb-8">
--- src/components/about/cover-section.tsx
+++ src/components/about/cover-section.tsx
@@ -5,25 +5,17 @@ type Props = {}
 
 export const CoverSection = (props: Props) => {
   return (
-    <section className="mx-10 flex h-[75hv]  flex-row items-center justify-center border-b-2 border-solid border-dark text-dark">
-      <div className="flex h-full flex-1 justify-center border-r-2 border-solid border-dark">
-        <Image
-          alt="Profile Image"
-          src={profileImage}
-          className="h-full w-full object-contain object-center"
-        />
+    <section className="border-dark text-dark mx-10 flex h-[75hv] flex-row items-center justify-center border-b-2 border-solid">
+      <div className="border-dark flex h-full flex-1 justify-center border-r-2 border-solid">
+        <Image alt="Profile Image" src={profileImage} className="h-full w-full object-contain object-center" />
       </div>
 
       <div className="flex flex-1 flex-col items-center justify-center px-16 text-left">
-        <h2 className="text-6xl font-bold capitalize">
-          Dream Big, Work Hard, Achieve More!
-        </h2>
+        <h2 className="text-6xl font-bold capitalize">Dream Big, Work Hard, Achieve More!</h2>
         <p className="mt-4 text-base font-medium capitalize">
-          This Mantra Drives My Work As A Passionate Freelancer. I Blend
-          Innovative Technology With Timeless Design For Captivating Digital
-          Experiences. Inspired By Nature And Literature, I&apos;m A Perpetual
-          Learner Embracing Challenges. With Each Project, I Aim To Leave A
-          Lasting Impact—One Pixel At A Time.
+          This Mantra Drives My Work As A Passionate Freelancer. I Blend Innovative Technology With Timeless Design For
+          Captivating Digital Experiences. Inspired By Nature And Literature, I&apos;m A Perpetual Learner Embracing
+          Challenges. With Each Project, I Aim To Leave A Lasting Impact—One Pixel At A Time.
         </p>
       </div>
     </section>
